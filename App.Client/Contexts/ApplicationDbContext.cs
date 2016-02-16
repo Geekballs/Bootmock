e4bfc3aca36553126b967e115a16d124cdffbe6f
Geekballs <james.mcgeachie.qdrive@gmail.com>
2016-02-16 20:42:17 +0000

    Renamed identity database schema.

--- App.Client/Contexts/ApplicationDbContext.cs
+++ App.Client/Contexts/ApplicationDbContext.cs
@@ -21,12 +21,12 @@ namespace App.Client.Contexts
         {
             base.OnModelCreating(db);
             db.Conventions.Remove<PluralizingTableNameConvention>();
-            db.Entity<IdentityUserClaim>().ToTable("UserClaim").Property(x => x.Id).HasColumnName("ClaimId");
-            db.Entity<IdentityUserRole>().ToTable("UserRole");
-            db.Entity<IdentityUserLogin>().ToTable("UserLogin");
-            db.Entity<IdentityRole>().ToTable("Role").Property(x => x.Id).HasColumnName("RoleId");
-            db.Entity<IdentityRole>().ToTable("Role").Property(x => x.Name).HasColumnName("Title");
-            db.Entity<ApplicationUser>().ToTable("User").Property(x => x.Id).HasColumnName("UserId");
+            db.Entity<IdentityUserClaim>().ToTable("Security-UserClaim").Property(x => x.Id).HasColumnName("ClaimId");
+            db.Entity<IdentityUserRole>().ToTable("Security-UserRole");
+            db.Entity<IdentityUserLogin>().ToTable("Security-UserLogin");
+            db.Entity<IdentityRole>().ToTable("Security-Role").Property(x => x.Id).HasColumnName("RoleId");
+            db.Entity<IdentityRole>().ToTable("Security-Role").Property(x => x.Name).HasColumnName("Title");
+            db.Entity<ApplicationUser>().ToTable("Security-User").Property(x => x.Id).HasColumnName("UserId");
    
         }
   
